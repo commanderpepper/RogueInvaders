ba0edb9407e2e7c9baf239785d8334bb8052839c
commanderpepepr <ahmadhumza7@gmail.com>
2019-12-25 20:25:23 -0500

    Initial commit

---
+++ desktop/src/commanderpepper/spaceinvaders/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package commanderpepper.spaceinvaders.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import commanderpepper.spaceinvaders.SpaceInvaders;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceInvaders(), config);
+	}
+}
