363644fec90a338022928dd8f490b93cb0b61447
commanderpepepr <ahmadhumza7@gmail.com>
2020-01-09 21:00:53 -0500

    I tried to add logic to move the enemy ships left and right but they don't do that yet.

--- core/src/commanderpepper/objects/EnemyShipController.kt
+++ core/src/commanderpepper/objects/EnemyShipController.kt
@@ -1,9 +1,9 @@
 package commanderpepper.objects
 
-
 import com.badlogic.gdx.Gdx
 import commanderpepper.interfaces.Drawable
 import commanderpepper.interfaces.Updatable
+import kotlin.math.abs
 
 private const val horizontalEnemyShipDistance = 35
 private const val verticalEnemyShipDistance = 45
@@ -11,6 +11,13 @@ private const val verticalEnemyShipDistance = 45
 class EnemyShipController : Drawable, Updatable {
 
     private val enemyShipList = mutableListOf<EnemyShip>()
+    private var enemyShipHorizontalSpeed = 1f
+
+    private var leftMostEnemyShipPosition = horizontalEnemyShipDistance.toFloat()
+    private var rightMostEnemyShipPosition = (Gdx.graphics.width - verticalEnemyShipDistance).toFloat()
+
+    private var leftEnemyShipLimit = horizontalEnemyShipDistance.toFloat()
+    private var rightEnemyShipLimit = (Gdx.graphics.width - 5).toFloat()
 
     override fun draw() {
         enemyShipList.forEach {
@@ -19,7 +26,15 @@ class EnemyShipController : Drawable, Updatable {
     }
 
     override fun update() {
+        enemyShipHorizontalSpeed = determineHorizontalSpeed(enemyShipHorizontalSpeed)
+        if (enemyShipHorizontalSpeed > 0) {
+            rightMostEnemyShipPosition += enemyShipHorizontalSpeed
+        } else {
+            leftMostEnemyShipPosition += enemyShipHorizontalSpeed
+        }
+
         enemyShipList.forEach {
+            it.xSpeed = enemyShipHorizontalSpeed
             it.update()
         }
     }
@@ -29,12 +44,24 @@ class EnemyShipController : Drawable, Updatable {
             for (column in 0..9) {
                 EnemyShip(horizontalEnemyShipDistance + column.toFloat() * horizontalEnemyShipDistance,
                         Gdx.graphics.height - row.toFloat() * verticalEnemyShipDistance,
-                        1f,
+                        0f,
                         0f).also {
                     enemyShipList.add(it)
                 }
             }
         }
+    }
 
+    fun determineHorizontalSpeed(horizontalSpeed: Float): Float {
+        var newSpeed = horizontalSpeed
+        //If true start going right
+        if (leftMostEnemyShipPosition == leftEnemyShipLimit) {
+            newSpeed = abs(horizontalSpeed)
+        }
+        //If true, start going left
+        if (rightMostEnemyShipPosition == rightEnemyShipLimit) {
+            newSpeed = horizontalSpeed * -1
+        }
+        return newSpeed
     }
 }
